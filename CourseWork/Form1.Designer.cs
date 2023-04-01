2ec7868c45b57d1a68d89f3706f947ab3b2fd599
Денис Кит <Ra40k_I3_Dotk1@bk.ru>
2023-04-01 11:41:55 +0600

    Добавьте файлы проекта.

---
+++ CourseWork/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace CourseWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CommonGameButton = new System.Windows.Forms.Button();
+            this.MultipleGameButton = new System.Windows.Forms.Button();
+            this.ControlGameButton = new System.Windows.Forms.Button();
+            this.ButtonOfCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FAQButton = new System.Windows.Forms.Button();
+            this.Text7 = new System.Windows.Forms.Label();
+            this.Text8 = new System.Windows.Forms.Label();
+            this.Text1 = new System.Windows.Forms.Label();
+            this.Text2 = new System.Windows.Forms.Label();
+            this.Text5 = new System.Windows.Forms.Label();
+            this.Text3 = new System.Windows.Forms.Label();
+            this.Text4 = new System.Windows.Forms.Label();
+            this.FAQControlGame = new System.Windows.Forms.Button();
+            this.FAQCommonGame = new System.Windows.Forms.Button();
+            this.FAQMultipleGame = new System.Windows.Forms.Button();
+            this.Text6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CommonGameButton
+            // 
+            this.CommonGameButton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CommonGameButton.Location = new System.Drawing.Point(326, 287);
+            this.CommonGameButton.Name = "CommonGameButton";
+            this.CommonGameButton.Size = new System.Drawing.Size(97, 28);
+            this.CommonGameButton.TabIndex = 0;
+            this.CommonGameButton.Text = "CommonGame";
+            this.CommonGameButton.UseVisualStyleBackColor = true;
+            // 
+            // MultipleGameButton
+            // 
+            this.MultipleGameButton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MultipleGameButton.Location = new System.Drawing.Point(223, 287);
+            this.MultipleGameButton.Name = "MultipleGameButton";
+            this.MultipleGameButton.Size = new System.Drawing.Size(97, 28);
+            this.MultipleGameButton.TabIndex = 1;
+            this.MultipleGameButton.Text = "MultipleGame";
+            this.MultipleGameButton.UseVisualStyleBackColor = true;
+            // 
+            // ControlGameButton
+            // 
+            this.ControlGameButton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ControlGameButton.Location = new System.Drawing.Point(120, 287);
+            this.ControlGameButton.Name = "ControlGameButton";
+            this.ControlGameButton.Size = new System.Drawing.Size(97, 28);
+            this.ControlGameButton.TabIndex = 2;
+            this.ControlGameButton.Text = "ControlGame";
+            this.ControlGameButton.UseVisualStyleBackColor = true;
+            // 
+            // ButtonOfCancel
+            // 
+            this.ButtonOfCancel.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonOfCancel.Location = new System.Drawing.Point(12, 12);
+            this.ButtonOfCancel.Name = "ButtonOfCancel";
+            this.ButtonOfCancel.Size = new System.Drawing.Size(97, 28);
+            this.ButtonOfCancel.TabIndex = 3;
+            this.ButtonOfCancel.Text = "Cancel";
+            this.ButtonOfCancel.UseVisualStyleBackColor = true;
+            this.ButtonOfCancel.Visible = false;
+            this.ButtonOfCancel.Click += new System.EventHandler(this.ButtonOfCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(211, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Лотерея";
+            // 
+            // FAQButton
+            // 
+            this.FAQButton.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FAQButton.Location = new System.Drawing.Point(391, 15);
+            this.FAQButton.Name = "FAQButton";
+            this.FAQButton.Size = new System.Drawing.Size(97, 28);
+            this.FAQButton.TabIndex = 5;
+            this.FAQButton.Text = "FAQ";
+            this.FAQButton.UseVisualStyleBackColor = true;
+            this.FAQButton.Click += new System.EventHandler(this.FAQButton_Click);
+            // 
+            // Text7
+            // 
+            this.Text7.AutoSize = true;
+            this.Text7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text7.Location = new System.Drawing.Point(8, 191);
+            this.Text7.Name = "Text7";
+            this.Text7.Size = new System.Drawing.Size(461, 23);
+            this.Text7.TabIndex = 6;
+            this.Text7.Text = "Игра подразумевает три режима игры: \"обычный режим\",";
+            this.Text7.Visible = false;
+            // 
+            // Text8
+            // 
+            this.Text8.AutoSize = true;
+            this.Text8.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text8.Location = new System.Drawing.Point(8, 214);
+            this.Text8.Name = "Text8";
+            this.Text8.Size = new System.Drawing.Size(442, 23);
+            this.Text8.TabIndex = 7;
+            this.Text8.Text = "\"контрольный режим\" и \"залповый режим\", подробнее:";
+            this.Text8.Visible = false;
+            // 
+            // Text1
+            // 
+            this.Text1.AutoSize = true;
+            this.Text1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text1.Location = new System.Drawing.Point(8, 53);
+            this.Text1.Name = "Text1";
+            this.Text1.Size = new System.Drawing.Size(442, 23);
+            this.Text1.TabIndex = 8;
+            this.Text1.Text = "Игра подразумевает собой формат лотерейных билетов";
+            this.Text1.Visible = false;
+            // 
+            // Text2
+            // 
+            this.Text2.AutoSize = true;
+            this.Text2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text2.Location = new System.Drawing.Point(8, 76);
+            this.Text2.Name = "Text2";
+            this.Text2.Size = new System.Drawing.Size(451, 23);
+            this.Text2.TabIndex = 9;
+            this.Text2.Text = "В билете есть четыре поля, два из которых выигрышных";
+            this.Text2.Visible = false;
+            // 
+            // Text5
+            // 
+            this.Text5.AutoSize = true;
+            this.Text5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text5.Location = new System.Drawing.Point(9, 145);
+            this.Text5.Name = "Text5";
+            this.Text5.Size = new System.Drawing.Size(479, 23);
+            this.Text5.TabIndex = 10;
+            this.Text5.Text = "Игра продолжается до тех пор, пока не выбрано пустое поле";
+            this.Text5.Visible = false;
+            // 
+            // Text3
+            // 
+            this.Text3.AutoSize = true;
+            this.Text3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text3.Location = new System.Drawing.Point(8, 99);
+            this.Text3.Name = "Text3";
+            this.Text3.Size = new System.Drawing.Size(480, 23);
+            this.Text3.TabIndex = 11;
+            this.Text3.Text = "Выиграшное поле отмечается зеленым цветом и знаком \"@\"";
+            this.Text3.Visible = false;
+            // 
+            // Text4
+            // 
+            this.Text4.AutoSize = true;
+            this.Text4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text4.Location = new System.Drawing.Point(8, 122);
+            this.Text4.Name = "Text4";
+            this.Text4.Size = new System.Drawing.Size(441, 23);
+            this.Text4.TabIndex = 12;
+            this.Text4.Text = "Проигрошное поле отмечается пустым красным полем";
+            this.Text4.Visible = false;
+            // 
+            // FAQControlGame
+            // 
+            this.FAQControlGame.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FAQControlGame.Location = new System.Drawing.Point(22, 258);
+            this.FAQControlGame.Name = "FAQControlGame";
+            this.FAQControlGame.Size = new System.Drawing.Size(151, 41);
+            this.FAQControlGame.TabIndex = 14;
+            this.FAQControlGame.Text = "FAQ контрольный режим";
+            this.FAQControlGame.UseVisualStyleBackColor = true;
+            this.FAQControlGame.Visible = false;
+            this.FAQControlGame.Click += new System.EventHandler(this.FAQControlGame_Click);
+            // 
+            // FAQCommonGame
+            // 
+            this.FAQCommonGame.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FAQCommonGame.Location = new System.Drawing.Point(179, 258);
+            this.FAQCommonGame.Name = "FAQCommonGame";
+            this.FAQCommonGame.Size = new System.Drawing.Size(151, 41);
+            this.FAQCommonGame.TabIndex = 15;
+            this.FAQCommonGame.Text = "FAQ обычный режим";
+            this.FAQCommonGame.UseVisualStyleBackColor = true;
+            this.FAQCommonGame.Visible = false;
+            this.FAQCommonGame.Click += new System.EventHandler(this.FAQCommonGame_Click);
+            // 
+            // FAQMultipleGame
+            // 
+            this.FAQMultipleGame.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FAQMultipleGame.Location = new System.Drawing.Point(334, 258);
+            this.FAQMultipleGame.Name = "FAQMultipleGame";
+            this.FAQMultipleGame.Size = new System.Drawing.Size(151, 41);
+            this.FAQMultipleGame.TabIndex = 16;
+            this.FAQMultipleGame.Text = "FAQ залповыый режим";
+            this.FAQMultipleGame.UseVisualStyleBackColor = true;
+            this.FAQMultipleGame.Visible = false;
+            this.FAQMultipleGame.Click += new System.EventHandler(this.FAQMultipleGame_Click);
+            // 
+            // Text6
+            // 
+            this.Text6.AutoSize = true;
+            this.Text6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Text6.Location = new System.Drawing.Point(8, 168);
+            this.Text6.Name = "Text6";
+            this.Text6.Size = new System.Drawing.Size(332, 23);
+            this.Text6.TabIndex = 17;
+            this.Text6.Text = "Или пока игрок не заберет свой выигрыш";
+            this.Text6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(497, 327);
+            this.Controls.Add(this.CommonGameButton);
+            this.Controls.Add(this.MultipleGameButton);
+            this.Controls.Add(this.ControlGameButton);
+            this.Controls.Add(this.Text6);
+            this.Controls.Add(this.FAQMultipleGame);
+            this.Controls.Add(this.FAQCommonGame);
+            this.Controls.Add(this.FAQControlGame);
+            this.Controls.Add(this.Text4);
+            this.Controls.Add(this.Text3);
+            this.Controls.Add(this.Text5);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.Text8);
+            this.Controls.Add(this.Text7);
+            this.Controls.Add(this.FAQButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonOfCancel);
+            this.Name = "Form1";
+            this.Text = "Лотерея";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CommonGameButton;
+        private System.Windows.Forms.Button MultipleGameButton;
+        private System.Windows.Forms.Button ControlGameButton;
+        private System.Windows.Forms.Button ButtonOfCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button FAQButton;
+        private System.Windows.Forms.Label Text7;
+        private System.Windows.Forms.Label Text8;
+        private System.Windows.Forms.Label Text1;
+        private System.Windows.Forms.Label Text2;
+        private System.Windows.Forms.Label Text5;
+        private System.Windows.Forms.Label Text3;
+        private System.Windows.Forms.Label Text4;
+        private System.Windows.Forms.Button FAQControlGame;
+        private System.Windows.Forms.Button FAQCommonGame;
+        private System.Windows.Forms.Button FAQMultipleGame;
+        private System.Windows.Forms.Label Text6;
+    }
+}
+
---
+++ CourseWork/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FAQButton_Click(object sender, EventArgs e)
+        {
+            ButtonOfCancel.Visible = true;
+            ControlGameButton.Visible = false;
+            CommonGameButton.Visible = false;
+            MultipleGameButton.Visible = false;
+            FAQButton.Visible = false;
+            FAQCommonGame.Visible = true;
+            FAQControlGame.Visible = true;
+            FAQMultipleGame.Visible = true;
+            Text1.Visible = true;
+            Text2.Visible = true;
+            Text5.Visible = true;
+            Text3.Visible = true;
+            Text4.Visible = true;
+            Text5.Visible = true;
+            Text6.Visible = true;
+            Text7.Visible = true;
+            Text8.Visible = true;
+        }
+
+        private void ButtonOfCancel_Click(object sender, EventArgs e)
+        {
+            ButtonOfCancel.Visible = false;
+            Text1.Visible = false;
+            Text2.Visible = false;
+            Text5.Visible = false;
+            Text3.Visible = false;
+            Text4.Visible = false;
+            Text5.Visible = false;
+            Text6.Visible = false;
+            Text7.Visible = false;
+            Text8.Visible = false;
+            ControlGameButton.Visible = true;
+            CommonGameButton.Visible = true;
+            MultipleGameButton.Visible = true;
+            FAQButton.Visible = true;
+            FAQCommonGame.Visible = false;
+            FAQControlGame.Visible = false;
+            FAQMultipleGame.Visible = false;
+
+        }
+
+        private void FAQCommonGame_Click(object sender, EventArgs e)
+        {
+
+            FAQCommonGame.BackColor = Color.Green;
+            FAQControlGame.BackColor = Color.White;
+            FAQMultipleGame.BackColor = Color.White;
+        }
+
+        private void FAQControlGame_Click(object sender, EventArgs e)
+        {
+            FAQCommonGame.BackColor = Color.White;
+            FAQControlGame.BackColor = Color.Green;
+            FAQMultipleGame.BackColor = Color.White;
+
+
+        }
+
+        private void FAQMultipleGame_Click(object sender, EventArgs e)
+        {
+            FAQCommonGame.BackColor = Color.White;
+            FAQControlGame.BackColor = Color.White;
+            FAQMultipleGame.BackColor = Color.Green;
+        }
+    }
+}
